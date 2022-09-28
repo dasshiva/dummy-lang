a5ed96ce38b24aac448ec8885efb4b1e292221b4
root <root@localhost.localdomain>
2022-09-28 16:13:09 +0000

    Working on parser

---
+++ Parser.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Lang {
+	public class Parser {
+	        private readonly Tokenizer src;
+	        // private bool inLabel;	
+		public Parser(Tokenizer src) {
+			this.src = src;
+		}
+
+		public void Parse() {
+			var read = src.ReadLine();
+			for (int i = 0; i < read.Length; i++) {
+				switch(read[i].Type) {
+					case Tokens.FSTART:
+					case Tokens.FEND:
+					{
+						if(read.Length < 2)
+							Exit("Error: Funtion name expected!");
+						if (read[i + 1].Type != Tokens.L_STRING)
+							Exit("Error: Functiom name must be a string!");
+						ParseFunction((string?) read[i + 1].Arg);
+					}
+
+				}
+			}
+		}
+
+		public void ParseFunction(string? name) {
+			var read = src.ReadLine();
+
+		}
+
+	}
+
+	public class ParserResult {
+		private string functionName;
+		// private 
+	}
+}
--- Tokenizer.cs
+++ Tokenizer.cs
@@ -3,6 +3,7 @@ using System;
 namespace Lang {
 	public enum Tokens {
 		INS_ADD, INS_SUB, INS_MUL, INS_DIV,
+		K_FSTART, K_FEND,
 		S_COMMA, S_SEMI_COL,
 		L_INT, L_SINT, L_DECIMAL, L_STRING,
 		F_EOF
@@ -11,17 +12,13 @@ namespace Lang {
 	public readonly record struct TokenizerResult (Tokens Type, object? Arg);
 
 	public class Tokenizer {
-		private readonly Reader src;
-
-		public Tokenizer(Reader src) {
-			this.src = src;
-		}
-
+		public Reader src { get; set; }
 		private TokenizerResult Check(string word) => word switch {
 			"add" => new TokenizerResult(Tokens.INS_ADD, null),
 			"sub" => new TokenizerResult(Tokens.INS_SUB, null),
 			"mul" => new TokenizerResult(Tokens.INS_MUL, null),
 			"div" => new TokenizerResult(Tokens.INS_DIV, null),
+			"function" => new TokenizerResult(Tokens.FSTART, null),
 			"," => new TokenizerResult(Tokens.S_COMMA, null),
 			";" => new TokenizerResult(Tokens.S_SEMI_COL, null),
 			_ => DealRest(word)
@@ -37,7 +34,7 @@ namespace Lang {
 			else if (Double.TryParse(word, out dnum))
 				return new TokenizerResult(Tokens.L_DECIMAL, dnum);
 			else if (word[word.Length - 1] == 'U') {
-				if(UInt64.TryParse(word, out unum))
+				if(UInt64.TryParse(word.SubString(0, word.Length - 2), out unum))
 					return new TokenizerResult(Tokens.L_SINT, unum);
 			}
 			return new TokenizerResult(Tokens.L_STRING, word);
