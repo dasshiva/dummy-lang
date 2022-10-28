ee8785d585fdb0b287a124ab0792b2391b9cb65e
Admin <root@localhost.localdomain>
2022-10-28 06:53:51 +0000

    More work on runtime

--- src/runtime/Insn.cpp
+++ src/runtime/Insn.cpp
@@ -2,26 +2,47 @@
 #include "include/Read.hpp"
 #include "include/Log.hpp"
 
-#define ADD_SUFFIX(dig) (dig == 1) ? ADD_USS : (dig == 2) ? ADD_SUS :C(dig == 3) ? ADD_SS : (dig == 4) ? ADD_USUS : (dig == 5) ? ADD_RS : (dig == 6) ? ADD_SR : (dig == 7) ? ADD_RUS : ADD_USR;
+#define SUFFIX(pre, dig) (dig == 1) ? USS_##pre : (dig == 2) ? SUS_##pre :(dig == 3) ? SS_##pre : (dig == 4) ? USUS_##pre : (dig == 5) ? RS_##pre : (dig == 6) ? SR_##pre : (dig == 7) ? RUS_##pre : (dig == 8) ? USR_##pre : 0;
+
 
 void Insn::Prepare(std::ifstream& file) {
 	ins = GetInsn(Read_U2(file));
-	len = GetArgLen();
+	// len = GetArgLen();
 	dest = Read_U1(file);
 
 }
 
-Insn _internal_getsuffix() {
-}
-
 Insns Insn::GetInsn(u2 code) {
+	Insns ret;
 	if (code > 0xC8)
 		LOG(fatal, "Illegal instruction %i", code);
-	if (code < 0x08) {
-		int l = code % 10;
-		return (l == 0) ? ADD_USS : ADD_SUS;
-	}
+
+	if (code < 0x08) 
+		ret = SUFFIX(ADD, code % 10)
+	else if (code > 0xA1 && code < 0xA9)
+		ret = SUFFIX(SUB, code % 10)
+	else if (code > 0xB1 && code < 0xB9)
+		ret = SUFFIX(MUL, code % 10)
+	else if (code > 0xC1)
+		ret = SUFFIX(DIV, code % 10)
+	else
+		LOG(fatal, "Illegal instruction %i", code);
+	if (ret == 0)
+		LOG(fatal, "Illegal instruction %i", code);
+	return ret;
 }
 
-
+Regs Insn::GetRegs(u1 code) {
+	switch (code) {
+		case 0xAA: return R1;
+		case 0xAB: return R2;
+		case 0xAC: return R3;
+		case 0xAD: return R4;
+		case 0xAE: return R5;
+		case 0xAF: return R6;
+		case 0xBA: return R7;
+		case 0xBB: return R8;
+		case 0xBC: return R9;
+	}
+}
 
--- src/runtime/include/Insn.hpp
+++ src/runtime/include/Insn.hpp
@@ -8,41 +8,56 @@
  * src/compiler/LangModel.cs */
 
 enum Insns {
-	ADD_USS = 0x01,
-	ADD_SUS = 0x02,
-  	ADD_SS = 0x03,                                           
-      	ADD_USUS = 0x04,
-  	ADD_RS = 0x05,
-    	ADD_SR = 0x06,
-  	ADD_RUS = 0x07,
-  	ADD_USR = 0x08,
+	USS_ADD = 0x01,
+	SUS_ADD = 0x02,
+  	SS_ADD = 0x03,
+	USUS_ADD = 0x04,
+  	RS_ADD = 0x05,
+    	SR_ADD = 0x06,
+  	RUS_ADD = 0x07,
+  	USR_ADD = 0x08,
 
-  	SUB_USS = 0xA1,
-  	SUB_SUS = 0xA2,
-  	SUB_SS = 0xA3,
-  	SUB_USUS = 0xA4,
-  	SUB_RS = 0xA5,
-  	SUB_SR = 0xA6,
-  	SUB_RUS = 0xA7,
-  	SUB_USR = 0xA8,
+  	USS_SUB = 0xA1,
+  	SUS_SUB = 0xA2,
+  	SS_SUB = 0xA3,
+  	USUS_SUB = 0xA4,
+  	RS_SUB = 0xA5,
+  	SR_SUB = 0xA6,
+  	RUS_SUB = 0xA7,
+  	USR_SUB = 0xA8,
 
-  	MUL_USS = 0xB1,
-  	MUL_SUS = 0xB2,
-  	MUL_SS = 0xB3,
-  	MUL_USUS = 0xB4,
-  	MUL_RS = 0xB5,
-  	MUL_SR = 0xB6,
-  	MUL_RUS = 0xB7,
-  	MUL_USR = 0xB8,
+  	USS_MUL = 0xB1,
+  	SUS_MUL = 0xB2,
+  	SS_MUL = 0xB3,
+  	USUS_MUL = 0xB4,
+  	RS_MUL = 0xB5,
+  	SR_MUL = 0xB6,
+  	RUS_MUL = 0xB7,
+  	USR_MUL = 0xB8,
 
-	DIV_USS = 0xC1,
-	DIV_SUS = 0xC2,
-  	DIV_SS = 0xC3,
-    	DIV_USUS = 0xC4,                                 
-	DIV_RS = 0xC5,
-  	DIV_SR = 0xC6,                                
-	DIV_RUS = 0xC7,                                
-	DIV_USR = 0xC8
+	USS_DIV = 0xC1,
+	SUS_DIV = 0xC2,
+  	SS_DIV = 0xC3,
+    	USUS_DIV = 0xC4,                                 
+	RS_DIV = 0xC5,
+  	SR_DIV = 0xC6,                                
+	RUS_DIV = 0xC7,                                
+	USR_DIV = 0xC8
+};
+
+enum Regs {
+      	R1 = 0xAA, 
+	R2 = 0xAB, 
+	R3 = 0xAC, 
+	R4 = 0xAD, 
+	R5 = 0xAE, 
+	R6 = 0xAF,
+	R7 = 0xBA, 
+	R8 = 0xBB, 
+	R9 = 0xBC, 
+	R10 = 0xBD, 
+	R11 = 0xBE, 
+	R12 = 0XBF
 };
 
 union Arg {
@@ -54,13 +69,14 @@ class Insn {
 	public:
 		Insns ins;
 		u1 len;
-		u1 dest;
+		Regs dest;
 		union Arg** args;
 
 		void Prepare(std::ifstream& f);
 	
 	private:
 		Insns GetInsn(u2 code);
+		Regs GetReg(u1 reg);
 		u1 GetArgLen();
 };
 
